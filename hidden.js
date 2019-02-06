2714ee01ee37c552e5e9e00cbec84e1599b208ca
GarrettJPeters <garrettskater99@yahoo.com>
2019-02-05 22:05:31 -0800

    added another game

--- hidden.js
+++ hidden.js
@@ -11,11 +11,26 @@ const keySequence = [
   'b',
   'a',
 ];
+const keySequence2 = [
+  'ArrowUp',
+  'ArrowUp',
+  'ArrowDown',
+  'ArrowDown',
+  'ArrowLeft',
+  'ArrowRight',
+  'ArrowLeft',
+  'ArrowRight',
+  'a',
+  'b',
+];
 let userInput = new Array( keySequence.length );
 window.addEventListener( 'keydown', ( { key } ) => {
 userInput = [ ...userInput.slice( 1 ), key ];
 
 if ( keySequence.every( ( v, k ) => v === userInput[ k ] ) ) {
   document.getElementById("hiddengame").className = "show"
+}
+if ( keySequence2.every( ( v, k ) => v === userInput[ k ] ) ) {
+  document.getElementById("hiddengame2").className = "show"
 }
 } );
